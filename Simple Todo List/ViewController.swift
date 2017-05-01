b91c378eb50a2940c776aed65ad2c2e235c13727
Rebeca Souza <rebeca@rebeca-2.local>
2017-05-01 08:59:59 -0400

    Initial Commit

---
+++ Simple Todo List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Todo List
+//
+//  Created by Rebeca Souza on 2017-05-01.
+//  Copyright © 2017 Rebeca Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
