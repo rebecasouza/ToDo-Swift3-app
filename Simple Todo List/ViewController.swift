a927e3646660b2918695fc9d99c1fcb542ed3f49
Rebeca Souza <rebeca@rebeca-2.local>
2017-05-01 15:27:14 -0400

    UI to to controller connections

--- Simple Todo List/ViewController.swift
+++ Simple Todo List/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var txtOutput: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func addItem(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
