3c8169528de76f31423a94c3462b708377acbfe6
Rebeca Souza <rebeca@rebeca-2.local>
2017-05-01 17:17:36 -0400

    Adding items to the list

--- Simple Todo List/ViewController.swift
+++ Simple Todo List/ViewController.swift
@@ -13,12 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtInput: UITextField!
     @IBOutlet weak var txtOutput: UITextView!
     
+    var items: [String] = []
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func addItem(_ sender: Any) {
+        if (txtInput.text == "") {
+            return
+        }
+        items.append(txtInput.text!)
+        txtOutput.text! = ""
+        
+        for item in items {
+            txtOutput.text.append("\(item)\n")
+        }
+        txtInput.text  = ""
+        txtInput.resignFirstResponder()
+    }
+    
+    // Event handling
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
